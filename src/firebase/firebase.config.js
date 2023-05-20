2e9cbeb25318232257d38fe7e0d90c995bb0d14d
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-20 21:12:07 +0600

    Firebase initialization .env.local file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBzurLSeT9ZdXzVjOHPbqPEFmo0C-NJHPs",
-  authDomain: "kidd-toy-client.firebaseapp.com",
-  projectId: "kidd-toy-client",
-  storageBucket: "kidd-toy-client.appspot.com",
-  messagingSenderId: "1012522189603",
-  appId: "1:1012522189603:web:32f23f756cc0e873730296",
+  apiKey: import.meta.envVITE_apiKey,
+  authDomain: import.meta.envVITE_authDomain,
+  projectId: import.meta.envVITE_projectId,
+  storageBucket: import.meta.envVITE_storageBucket,
+  messagingSenderId: import.meta.envVITE_messagingSenderId,
+  appId: import.meta.envVITE_appId,
 };
 
 // Initialize Firebase
