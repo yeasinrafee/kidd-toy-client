992274cc2dba4cd30ccd9b077659158f84819dd5
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-20 13:51:29 +0600

    Footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Outlet />
+      <Footer />
     </div>
   );
 };
