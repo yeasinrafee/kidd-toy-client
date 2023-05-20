62c33a36920a20a7aa1a015e9fc555015bcfc1b8
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-20 13:05:20 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-bold text-5xl text-red-300">Hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
+);
