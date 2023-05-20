a1506946c1b26090ccfdc8b58435dbe45d828284
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-20 15:43:37 +0600

    Navbar without using user

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,69 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+  const navMenu = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/allToys">All Toys</Link>
+      </li>
+      <li>
+        <Link to="/myToys">My Toys</Link>
+      </li>
+      <li>
+        <Link to="/addToys">Add A Toy</Link>
+      </li>
+      <li>
+        <Link to="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link to="/user">User</Link>
+      </li>
+
+      <li>
+        <button>Log out</button>
+      </li>
+      {/* <li>
+        <Link to="/login">Login</Link>
+      </li> */}
+    </>
+  );
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="navbar h-28 mb-5 bg-violet-500 text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navMenu}
+          </ul>
+        </div>
+        <Link className="btn btn-ghost normal-case">
+          <h2 className="text-4xl font-bold mb-5">KiddToY</h2>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navMenu}</ul>
+      </div>
     </div>
   );
 };
