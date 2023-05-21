58a0b78131878d7ed7a07e6c76ffc3c28b4ad1fd
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-21 11:35:16 +0600

    Contact section

---
+++ src/pages/Home/Contact/Contact.jsx
@@ -0,0 +1,37 @@
+import { FaClock, FaMapMarkedAlt, FaMobile } from "react-icons/fa";
+
+const Contact = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-around bg-violet-500 text-white mt-20 mb-10 py-14 rounded-md">
+      <div className="flex items-center gap-5 mb-9 md:mb-0">
+        <p className="text-4xl bg-violet-700 p-3 rounded-lg">
+          <FaClock></FaClock>
+        </p>
+        <div>
+          <p>We are open Saturday-Friday</p>
+          <h3 className="text-2xl font-bold">10:00 am - 11:30 pm</h3>
+        </div>
+      </div>
+      <div className="flex items-center gap-5 mb-9 md:mb-0">
+        <p className="text-4xl bg-violet-700 p-3 rounded-lg">
+          <FaMobile></FaMobile>
+        </p>
+        <div>
+          <p>Wanna know more?</p>
+          <h3 className="text-2xl font-bold">+880 454 345 2345</h3>
+        </div>
+      </div>
+      <div className="flex items-center gap-5">
+        <p className="text-4xl bg-violet-700 p-3 rounded-lg">
+          <FaMapMarkedAlt></FaMapMarkedAlt>
+        </p>
+        <div>
+          <p>Visit Our Shop, Address: </p>
+          <h3 className="text-2xl font-bold">Uttara, Dhaka-1230</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import AboutUs from "../AboutUs/AboutUs";
 import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
 import PhotoGallery from "../PhotoGallery/PhotoGallery";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner />
       <PhotoGallery />
+      <Contact />
       <AboutUs />
     </div>
   );
