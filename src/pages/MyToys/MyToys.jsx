019ba39686684516a728968a821b98852f25cbff
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-21 21:42:47 +0600

    Delete added

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const MyToys = () => {
   const { user } = useContext(AuthContext);
@@ -12,6 +13,32 @@ const MyToys = () => {
         setMyToys(data);
       });
   }, [user]);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/myToys/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              console.log(data);
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="px-10 py-20">
       <h2 className="text-3xl text-violet-500 mt-5 mb-9">My Toys List: </h2>
@@ -33,7 +60,7 @@ const MyToys = () => {
           {/* row 1 */}
           {myToys &&
             myToys.map((toys, index) => (
-              <tr key={toys._id}>
+              <tr key={toys?._id}>
                 <th>{index + 1}</th>
                 <td>
                   <div className="flex items-center space-x-3">
@@ -46,16 +73,19 @@ const MyToys = () => {
                 <td>{toys?.sub_category}</td>
                 <td>$ {toys?.price}</td>
                 <td>{toys?.quantity}</td>
-                <th>
+                <td>
                   <button className="btn bg-violet-500 btn-xs border-none">
-                    Update
+                    <label htmlFor="my-modal">update</label>
                   </button>
-                </th>
-                <th>
-                  <button className="btn bg-violet-500 btn-xs border-none">
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(toys?._id)}
+                    className="btn bg-violet-500 btn-xs border-none"
+                  >
                     Delete
                   </button>
-                </th>
+                </td>
               </tr>
             ))}
         </tbody>
